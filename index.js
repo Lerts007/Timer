1b3d0060033d9caa285fc8427102ce8b661efa30
Lerts <oddff@mail.ru>
2023-03-21 23:10:26 +0300

    Таймер ведет обратный отсчет, в input разрешен ввод  только цифр

--- index.js
+++ index.js
@@ -1,24 +1,38 @@
-const inputEl = document.querySelector('input');
-const buttonEl = document.querySelector('button');
-const timerEl = document.querySelector('span');
+const inputEl = document.querySelector("input");
+const buttonEl = document.querySelector("button");
+const timerEl = document.querySelector("span");
 
 // Напишите реализацию createTimerAnimator
 // который будет анимировать timerEl
 const createTimerAnimator = () => {
-  return (seconds) => {};
+  return (seconds) => {
+    let timer = setInterval(() => {
+      let hour = Math.floor(seconds / 3600);
+      let minutes = Math.floor((seconds - hour * 3600) / 60);
+      let second = seconds % 60;
+
+      hour = hour < 10 ? "0" + hour : hour;
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      second = second < 10 ? "0" + second : second;
+
+      timerEl.innerHTML = `${hour}:${minutes}:${second}`;
+
+      if (seconds == 0) clearTimeout(timer);
+      else seconds--;
+    }, 1000);
+  };
 };
 
 const animateTimer = createTimerAnimator();
 
-inputEl.addEventListener('input', () => {
-  // Очистите input так, чтобы в значении
-  // оставались только числа
+inputEl.addEventListener("input", (e) => {
+  e.target.value = e.target.value.replace(/\D/g, "");
 });
 
-buttonEl.addEventListener('click', () => {
+buttonEl.addEventListener("click", () => {
   const seconds = Number(inputEl.value);
 
   animateTimer(seconds);
 
-  inputEl.value = '';
+  inputEl.value = "";
 });
